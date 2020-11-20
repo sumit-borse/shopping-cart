16805a95d714c7327d722ad3ea567f3dd3a22765
sumit-borse <sumit.borse@bluepineapple.io>
2020-11-20 12:26:48 +0530

    Complete Dashboard Design, Just littel bit CSS part is reamining

--- JavaScript/dashboard.js
+++ JavaScript/dashboard.js
@@ -2,4 +2,88 @@ window.onload = () =>{
     var nameToDisplay = document.querySelector("h3#userName");
     var storedUserDetails = JSON.parse(localStorage.getItem("userDetails"));   
     nameToDisplay.innerHTML = "Welcome  " + storedUserDetails[0];
+    var items = JSON.parse(localStorage.getItem("names"));
+
+    for(i=0;i<items.length;i++){
+        var node = document.createElement("div");
+        node.className="item-detail-div";
+
+        var leftDiv = document.createElement("div");
+        leftDiv.className="left-div";
+        var itemImage = document.createElement("img");
+        itemImage.className ="item-image"
+        itemImage.src = "../Images/"+items[i].imgName;
+        leftDiv.appendChild(itemImage);
+        node.appendChild(leftDiv);
+
+
+        var middleDiv = document.createElement("div");
+        middleDiv.className="middle-div";
+
+        var itemName = document.createElement("a");
+        itemName.href="#";
+        itemName.textContent= items[i].name;
+        itemName.className="item-name";
+
+        var details=items[i].highlights;
+
+        var highlightsList = document.createElement("ul");
+        highlightsList.className="highlights-list";
+        var listItem ;
+        details.forEach(element => {
+            listItem=document.createElement("li");
+            listItem.textContent=element;
+            listItem.className="list-items"
+            highlightsList.appendChild(listItem);
+        });
+
+        middleDiv.appendChild(itemName);
+        middleDiv.appendChild(highlightsList);
+        node.appendChild(middleDiv);
+
+
+        var rightDiv = document.createElement("div");
+        rightDiv.className = "right-div";
+
+        var mrp = document.createElement("h3");
+        mrp.className="mrp";
+        var actualPrice = document.createElement("h2");
+        actualPrice.className="actual-price"
+
+        var priceDiv = document.createElement('div');
+        priceDiv.className="mrp-discount";
+
+        var price=items[i].price;
+        var newPrice=price.replace(',','');
+        var discount=items[i].discount;
+        var priceAfterDiscount =newPrice-((newPrice*discount)/100).toPrecision(2);
+        var disc= document.createElement("h2");
+        disc.className="discount";
+        disc.textContent=discount+"%";
+
+        mrp.textContent=newPrice;
+        actualPrice.textContent=priceAfterDiscount;
+
+        rightDiv.appendChild(actualPrice);
+        priceDiv.appendChild(mrp);
+        priceDiv.appendChild(disc);
+        rightDiv.appendChild(priceDiv);
+        node.appendChild(rightDiv);
+
+        document.getElementById("container").appendChild(node);
+    }
+}
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset < sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,7 +1,7 @@
 const items = [
     {
         name : "Tima Cricket Kit Bag Canvas Kit Bag, 70cm x 33 cm x 19cm (Blue/Black) Kit Bag  (Blue, Kit Bag)",
-        price : "₹1,399",
+        price : "1,399",
         discount : "40",
         reating : "3",
         highlights : ["Bag Capacity: 43.89 L","Cricket Bag","Style: Kit Bag","Material: Nylon Braided Polyester with high quality zips. Padded adjustable Shoulder Straps & Hanger."],
@@ -10,16 +10,16 @@ const items = [
     },
     {
         name : "Tima Cricket Windball Training Cricket Ball (Pack of 6) Cricket Tennis Ball  (Pack of 6, Multicolor)",
-        price : "₹999",
+        price : "999",
         discount : "40",
         reating : "4",
         highlights : ["Cricket Tennis Ball","Water Resistant","Outer Material: Made from a complex mixture of polyester and rubber compound these imparts excellent finish strength and resistant to wear & tear due to rough usage.","Core Material: Wind","Weight: 75-85 g"],
         description : "It is an soft touch cricket ball for gully cricket, game of catch and throw, this pack of 6 soft cricket balls from tima international to make your game enjoyable and exciting. Rubber each of these balls is made from the finest quality rubber, these colourful balls are great for your daily games with friends catch and throw, backyard cricket or any other game of your fun.",
-        imgName : "cricketBall.jpeg"
+        imgName : "cricktBall.jpeg"
     },
     {
         name : "CEAT HITMEN HIMACHAL WILLOW BAT 140 Poplar Willow Cricket Bat  (0.950 kg)",
-        price : "₹1,099",
+        price : "1,099",
         discount : "45",
         reating : "5",
         highlights : ["Age Group 15+ Yrs","Blade Made of Poplar Willow","Advanced Playing Level","Bat Grade: Grade 2","Sport Type: Cricket","With Cover","Weight Range 0.950 kg"],
@@ -28,7 +28,7 @@ const items = [
     },
     {
         name : "Sun Fly Club Cricket Leather Ball  (Pack of 2, Multicolor)",
-        price : "₹999",
+        price : "999",
         discount : "35",
         reating : "4",
         highlights : ["Cricket Leather Ball","Water Resistant","Outer Material: Genuine Leather","Weight: 156 g"],
@@ -37,7 +37,7 @@ const items = [
     },
     {
         name : "IBEX Champ Wicket Keeping Gloves Combo With Inner Gloves (Multicolor) Wicket Keeping Gloves  (Multicolor)",
-        price : "₹1,299",
+        price : "1,299",
         discount : "60",
         reating : "4",
         highlights : ["Left & Right Gloves","For Men, Boys, Men & Women","For Cricket","Weight: 200 g","Material: PU Foam"],
@@ -46,7 +46,7 @@ const items = [
     },
     {
         name : "FS Guard Sn.10 Abdominal Guard  (Yellow)",
-        price : "₹599",
+        price : "599",
         discount : "70",
         reating : "3",
         highlights : ["Type: Abdominal Guard","Ideal For: Men","For: Cricket","XL Size","XL Size","Left & Right Orientation"],
@@ -55,7 +55,7 @@ const items = [
     },
     {
         name : "SG Club XS (27 - 29 cm) Batting Pad  (White, Right Handed)",
-        price : "₹1,399",
+        price : "1,399",
         discount : "10",
         reating : "3.8",
         highlights : ["Type: Batting Pad", "Color: White","Inner Material: Cashmilon","Face Material: Polyvinyl Chloride","Weight: 750 g"],
@@ -64,19 +64,19 @@ const items = [
     },
     {
         name : "MANCO mEnwalkr ENDOS Thigh Guards Cricket Thigh Guard ",
-        price : "₹2,199",
+        price : "2,199",
         discount : "65",
         reating : "4",
         highlights : ["Type: Cricket Thigh Guard","Ideal For: Men, Boys, Senior","For: Cricket","Standard Size","NA Orientation"],
         description : "Extremely light weight,Flexible snug fit design for all thigh sizes 3D molded composite material for superior protection Hinged design for a better fit and eases running",
         imgName : "thighGuard.jpeg"
-    },
+    }
 ];
 
 
 
 window.onload = () => {
-    localStorage.setItem("localItems", JSON.stringify(items));
+    localStorage.setItem("names", JSON.stringify(items));
 };
 
 updateUserDetails = () =>{
